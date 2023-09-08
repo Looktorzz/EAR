a2555e2fd88202da86db0dc97be01735c5d2ab3a
Looktorzz <kulisutt.j@gmail.com>
2023-09-08 23:59:20 +0700

    Countdown time

--- Ear/Assets/Scripts/Interact/Interactable/IDoor.cs
+++ Ear/Assets/Scripts/Interact/Interactable/IDoor.cs
@@ -2,11 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class IDoor : MonoBehaviour , IInteractable
+public class IDoor : MonoBehaviour , IInteractable , IHoldInteractable
 {
     [SerializeField] private string _prompt;
     public string InteractionPrompt => _prompt;
     private Animator _animator;
+    
+    private float _currentTime = 2;
+    private float _startTime = 2;
+    private bool _isCount = false;
+    private bool _isCountComplete = false;
 
     public bool Interact(Interactor interactor)
     {
@@ -28,10 +33,71 @@ public class IDoor : MonoBehaviour , IInteractable
         return false;
 
     }
-
     
     private void Start()
     {
         _animator = GetComponent<Animator>();
+    }
+    
+    public void FixedUpdate()
+    {
+        if (_isCount)
+        {
+            _currentTime -= 1 * Time.deltaTime;
+            Debug.Log($"Time : {_currentTime:0}");
+            _isCountComplete = false;
+            
+            if (_currentTime <= 0)
+            {
+                _currentTime = 0;
+                _isCount = false;
+                _isCountComplete = true;
+                
+                Debug.Log("YOU CAN OPEN DOOR!");
+                _animator.SetTrigger("DoorOpen");
+            }
+        }
+    }
+
+    public void CountdownTime(bool isTrue)
+    {
+        if (isTrue)
+        {
+            if (!_isCount)
+            {
+                _currentTime = _startTime;
+                _isCount = true;
+            }
+        }
+        else
+        {
+            _isCount = false;
+        }
+
+    }
+
+    public bool HoldInteract(Interactor interactor)
+    {
+        var Door = interactor.GetComponentInChildren<Keys>();
+
+        if (Door == null)
+        {
+            Debug.Log("YOU HAVE NO KEY TO OPEN!");
+            return false;
+        }
+
+        if (Door.HasKey)
+        {
+            CountdownTime(true);
+            return true;
+        }
+
+        return false;
+    }
+
+    public bool ReleasedInteract(Interactor interactor)
+    {
+        CountdownTime(false);
+        return false;
     }
 }
--- Ear/Assets/Scripts/Interact/Interactor.cs
+++ Ear/Assets/Scripts/Interact/Interactor.cs
@@ -10,14 +10,8 @@ public class Interactor : MonoBehaviour
     [SerializeField] private float _interactionPointRadius = 0.5f;
     [SerializeField] private LayerMask _interactableMask;
 
-    public /*readonly*/ Collider[] _colliders = new Collider[3];
+    private readonly Collider[] _colliders = new Collider[3];
     [SerializeField] private int _numFound;
-
-    [SerializeField] private PlayerController _playerController;
-    private float _currentTime = 3;
-    private float _startTime = 3;
-    private bool _isCount = false;
-    private bool _isCountComplete = false;
 
     void Update()
     {
@@ -26,60 +20,6 @@ public class Interactor : MonoBehaviour
         _interactionPointRadius, _colliders, _interactableMask);
     }
 
-    public void FixedUpdate()
-    {
-        if (_isCount)
-        {
-            _currentTime -= 1 * Time.deltaTime;
-            Debug.Log($"Time : {_currentTime:0}");
-            _isCountComplete = false;
-            
-            if (_currentTime <= 0)
-            {
-                _currentTime = 0;
-                _isCount = false;
-                _isCountComplete = true;
-            }
-        }
-    }
-
-    public void CountdownTime(bool isTrue)
-    {
-        if (isTrue)
-        {
-            if (!_isCount)
-            {
-                _currentTime = _startTime;
-                _isCount = true;
-            }
-        }
-        else
-        {
-            _isCount = false;
-        }
-
-    }
-
-    public void HoldInteract()
-    {
-        _numFound = Physics.OverlapSphereNonAlloc(_interactionPoint.position, 
-            _interactionPointRadius, _colliders, _interactableMask);
-
-        if(_numFound > 0)
-        {
-            var interactable = _colliders[0].GetComponent<IInteractable>();
-
-            if(interactable != null && _isCountComplete)
-            {
-                interactable.Interact(this);
-            } 
-            /*if(interactable != null)
-            {
-                interactable.Interact(this);
-            } */
-        }
-    }
-    
     public void PressInteract()
     {
         _numFound = Physics.OverlapSphereNonAlloc(_interactionPoint.position, 
