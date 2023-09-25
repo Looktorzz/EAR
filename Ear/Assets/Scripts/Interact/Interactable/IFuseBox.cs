847fc4f724507e17b1a03353517a387f5fa1cfdf
Looktorzz <kulisutt.j@gmail.com>
2023-09-26 02:11:41 +0700

    Add FuseBox

--- Ear/Assets/Scripts/Interact/Interactable/IFuseBox.cs
+++ Ear/Assets/Scripts/Interact/Interactable/IFuseBox.cs
@@ -6,9 +6,25 @@ public class IFuseBox : MonoBehaviour, IInteractable
 {
     [SerializeField] private string _prompt;
     public string InteractionPrompt => _prompt;
-
+    
+    [SerializeField] private Transform _fusePosition;
+    public bool _isHaveFuse = false;
+    
     public bool Interact(Interactor interactor)
     {
+        Fuse fuse = interactor.GetComponentInChildren<Fuse>();
+        Debug.Log("Test Click Lever");
+
+        if (fuse != null)
+        {
+            interactor.GetComponentInChildren<Item>().PlaceItemOnInteract();
+            fuse.transform.SetParent(_fusePosition);
+            fuse.transform.localPosition = Vector3.zero;
+            _isHaveFuse = true;
+            Debug.Log("Complete Fill Fuse.");
+            
+            return true;
+        }
 
         return false;
     }
