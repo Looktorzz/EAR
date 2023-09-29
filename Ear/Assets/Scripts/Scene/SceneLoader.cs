00efccf4ef3025ad93cadd706b8275c64438b1ce
TanawatJira <97783368+Delphiros@users.noreply.github.com>
2023-09-29 23:34:44 +0700

    Add Scene Transition + Main Menu

---
+++ Ear/Assets/Scripts/Scene/SceneLoader.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+
+    [SerializeField]private Animator _anim;
+
+    public void StartGame()
+    {
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+
+    IEnumerator LoadLevel(int LevelIndex)
+    {
+        _anim.SetTrigger("Start");
+
+        yield return new WaitForSeconds(1);
+
+        SceneManager.LoadScene(LevelIndex);
+    }
+    
+    
+}
