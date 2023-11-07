5e82a1fc13e5c32bc0dcbbaa5ae8b0b4d2203bea
ArmNewbyDev <tanapat.rojs@gmail.com>
2023-11-08 00:21:05 +0700

    Add sound and some animation to scene Lift
    - Need Emp spirte
    - Need Player Dead (Lay on floor)

---
+++ Ear/Assets/Arts/Animation/PlayerAnimation/BlinkEye/LevelBefore2_Animation_Player.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelBefore2_Animation_Player : MonoBehaviour
+{
+    [SerializeField] Animator _animation;
+
+
+    public void CloseEye()
+    {
+        _animation.SetTrigger("Close_Eye");
+    }
+
+    public void BlinkEye()
+    {
+        _animation.SetTrigger("Blink_Eye");
+    }
+
+}
