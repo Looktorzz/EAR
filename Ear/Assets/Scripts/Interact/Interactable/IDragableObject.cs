0ef3f97e09e28a5966b91e05af715f18f9e93b05
Looktorzz <kulisutt.j@gmail.com>
2023-11-17 22:53:10 +0700

    IDragableObject Update

--- Ear/Assets/Scripts/Interact/Interactable/IDragableObject.cs
+++ Ear/Assets/Scripts/Interact/Interactable/IDragableObject.cs
@@ -6,18 +6,19 @@ using UnityEngine;
 
 public class IDragableObject : MonoBehaviour, IHoldGrabItem
 {
-    private BoxCollider _boxColliderAtPlayer;
-    private BoxCollider _boxColliderAtBox;
     private Rigidbody _rb;
     private bool _isDragNow = false;
+
+    private FixedJoint _fixedJoint;
+    private PlayerController _playerController;
 
     [SerializeField] private bool isBasin;
     [SerializeField] private bool isBox;
 
     private void Start()
     {
-        _boxColliderAtBox = GetComponent<BoxCollider>();
         _rb = GetComponent<Rigidbody>();
+        _playerController = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
     }
 
     public bool HoldInteract(Item item)
@@ -28,15 +29,15 @@ public class IDragableObject : MonoBehaviour, IHoldGrabItem
         {
             // ++Animation drag
             player.GetComponent<Item>().PlaceItem();
-            player.GetComponent<PlayerController>().isFreezeHand = true;
+            _playerController.isFreezeHand = true;
 
             _isDragNow = true;
-            transform.parent = player.transform;
-            Debug.Log("Check Drag Now");
+
+            _rb.constraints = RigidbodyConstraints.FreezeRotation;
+            _fixedJoint = player.AddComponent<FixedJoint>();
+            _fixedJoint.connectedBody = _rb;
             
-            CreatePlayerBoxCollider(player);
-            _rb.constraints = RigidbodyConstraints.FreezeAll;
-            _boxColliderAtBox.isTrigger = true;
+            Debug.Log("Check Drag Now");
             
             return true;
         }
@@ -49,27 +50,7 @@ public class IDragableObject : MonoBehaviour, IHoldGrabItem
         Debug.LogWarning("They are No Hold Interact");
         return false;
     }
-
-    void CreatePlayerBoxCollider(GameObject player)
-    {
-        _boxColliderAtPlayer = player.AddComponent<BoxCollider>();
-        
-        Vector3 boxSize = _boxColliderAtBox.size;
-        Vector3 boxCenter = _boxColliderAtBox.center;
-        Vector3 boxLocalScale = transform.localScale;
-        Vector3 boxLocalPos = transform.localPosition;
-            
-        _boxColliderAtPlayer.size = new Vector3(
-            boxSize.x * boxLocalScale.x,
-            boxSize.y * boxLocalScale.y,
-            boxSize.z * boxLocalScale.z);
-
-        _boxColliderAtPlayer.center = new Vector3(
-            boxCenter.x + boxLocalPos.x, 
-            ( boxCenter.y + boxLocalPos.y + player.transform.localPosition.y ) * 1.5f,
-            boxCenter.z + boxLocalPos.z);
-    }
-
+    
     public void HoldCompleteInteract()
     {
         throw new System.NotImplementedException();
@@ -77,20 +58,20 @@ public class IDragableObject : MonoBehaviour, IHoldGrabItem
 
     public bool ReleasedInteract(Item item)
     {
-        GameObject player = item.gameObject;
-        
         if(_isDragNow)
         {
             // --Animation drag
-            player.GetComponent<PlayerController>().CheckDurationAnimation("IsHoldDrag", .25f, false);
-            player.GetComponent<PlayerController>().isFreezeHand = false;
+            _playerController.CheckDurationAnimation("IsHoldDrag", .25f, false);
+            _playerController.isFreezeHand = false;
 
-            Destroy(_boxColliderAtPlayer);
-            _boxColliderAtBox.isTrigger = false;
-            _rb.constraints = ~RigidbodyConstraints.FreezePositionY;
+            if (_fixedJoint != null)
+            {
+                Destroy(_fixedJoint);
+            }
             
+            _rb.constraints = ~RigidbodyConstraints.FreezePositionY;
             _isDragNow = false;
-            transform.parent = null;
+            
             Debug.Log("Check Can't Drag Now");
 
             return true;
